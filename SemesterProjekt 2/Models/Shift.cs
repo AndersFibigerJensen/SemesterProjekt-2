15c4f6129a21a27a09d6a0b6944ffbae17a35ee3
wamas <wamas@LUNANIA>
2023-03-24 12:51:40 +0100

    test

---
+++ SemesterProjekt 2/Models/Shift.cs
@@ -0,0 +1,16 @@
+﻿namespace SemesterProjekt_2.Models
+{
+    public class Shift
+    {
+        private static int _shiftID;
+        private DateTime _dateFrom;
+        private DateTime _dateTo;
+        private Member _assignedMember;
+
+        public Shift(DateTime DateFrom, DateTime DateTo, Member AssignedMember)
+        {
+            _shiftID++;
+        }
+
+    }
+}
