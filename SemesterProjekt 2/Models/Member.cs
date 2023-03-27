acc65ed30f094621d4917d6e193047218ad5defb
Mammon22 <anders99fib@gmail.com>
2023-03-27 10:30:15 +0200

    Shift

--- SemesterProjekt 2/Models/Member.cs
+++ SemesterProjekt 2/Models/Member.cs
@@ -5,5 +5,22 @@
 
         public int MedlemID { get; set; }
 
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool isFamily { get; set; }
+
+        public List<T> shifts { get; set; }
+
+        public bool isAdmin  { get; set; }
+
+        public Member()
+        {
+
+        }
+
     }
 }
