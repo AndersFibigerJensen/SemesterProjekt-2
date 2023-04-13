6f2fb6f87a504d2e2a26a981b0ba558dc8b39033
Mammon22 <anders99fib@gmail.com>
2023-04-13 12:46:21 +0200

    memberchanges

--- SemesterProjekt 2/Pages/Members/GetAllMembers.cshtml.cs
+++ SemesterProjekt 2/Pages/Members/GetAllMembers.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SemesterProjekt_2.Pages.Medlemmer
+namespace SemesterProjekt_2.Pages.Members
 {
-    public class AddMembersModel : PageModel
+    public class GetAllMembersModel : PageModel
     {
         public void OnGet()
         {
