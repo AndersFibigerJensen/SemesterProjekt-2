48afa27b38ce645b4ef424de0f41ae63f07972b0
wamas <wamas@LUNANIA>
2023-04-13 09:44:06 +0200

    shift service skeleton

--- SemesterProjekt 2/Interfaces/IShiftService.cs
+++ SemesterProjekt 2/Interfaces/IShiftService.cs
@@ -1,8 +1,22 @@
-﻿namespace SemesterProjekt_2.Interfaces
+﻿using SemesterProjekt_2.Models;
+
+namespace SemesterProjekt_2.Interfaces
 {
 
-    //Shift
+    //Luna
     public interface IShiftService
     {
+        public Task<List<Shift>> GetAllShiftsAsync();
+
+        public Task<Shift> GetShiftByIdAsync(int id);
+
+        public Task AddShiftAsync();
+
+        public Task DeleteShiftAsync(int id);
+
+        public Task UpdateShiftAsync(int id);
+
+        public Task<List<Member>> FilterShiftsAsync(DateTime dtFrom, DateTime dtTo);
+
     }
 }
