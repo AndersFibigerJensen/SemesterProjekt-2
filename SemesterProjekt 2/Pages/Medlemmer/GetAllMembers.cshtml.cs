33398a8718dcdf6160684113e9dd047931a2623e
Mammon22 <anders99fib@gmail.com>
2023-04-13 11:13:26 +0200

    Member

--- SemesterProjekt 2/Pages/Medlemmer/GetAllMembers.cshtml.cs
+++ SemesterProjekt 2/Pages/Medlemmer/GetAllMembers.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SemesterProjekt_2.Interfaces;
+using SemesterProjekt_2.Models;
 
 namespace SemesterProjekt_2.Pages.Medlemmer
 {
     public class GetAllMembersModel : PageModel
     {
-        public void OnGet()
+
+        public List<Member> Members { get; set; }
+
+        private IMemberService _memberService;
+
+        public GetAllMembersModel(IMemberService memberservice)
         {
+            memberservice = _memberService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Members=await _memberService.GetAllMembersAsync();
         }
     }
 }
