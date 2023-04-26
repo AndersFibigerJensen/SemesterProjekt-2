5eb51d9c54d6fd9022588f37b8d8b4f0fca8d3c4
Mammon22 <anders99fib@gmail.com>
2023-04-26 10:13:41 +0200

    Login

---
+++ SemesterProjekt 2/Pages/Login/Login.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SemesterProjekt_2.Models;
+using SemesterProjekt_2.Interfaces;
+using SemesterProjekt_2.Services;
+
+namespace SemesterProjekt_2.Pages.Login
+{
+    public class LoginModel : PageModel
+    {
+        private IMemberService _memberService;
+        private LoginService _loginService;
+
+        [BindProperty]
+        public Member member { get; set; }
+
+        public LoginModel(IMemberService memberService, LoginService loginService)
+        {
+             _memberService= memberService;
+            _loginService = loginService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            List<Member> members = await _memberService.GetAllMembersAsync();
+            foreach(Member user in members)
+            {
+                if(user.Email==member.Email & user.Password==member.Password)
+                {
+                    _loginService.userlogin(user);
+                    return RedirectToPage("/Members/GetAllMembers");
+                }
+
+            }
+            return Page();
+        }
+    }
+}
--- SemesterProjekt 2/Services/LoginService.cs
+++ SemesterProjekt 2/Services/LoginService.cs
@@ -1,6 +1,28 @@
-﻿namespace SemesterProjekt_2.Services
+﻿using SemesterProjekt_2.Models;
+
+namespace SemesterProjekt_2.Services
 {
     public class LoginService
     {
+        private Member _user;
+
+        public async Task userlogin(Member user)
+        {
+            _user= user;
+        }
+
+        public async Task UserLoginAsync(Member user) 
+        {
+            _user = null;
+        }
+
+        public async Task<Member> GetLogged()
+        {
+            return _user;
+        }
+
+
+
+
     }
 }
