02e255ee31ff4b0005ed6b06310322dc7d0ae82b
Mammon22 <anders99fib@gmail.com>
2023-05-01 09:24:32 +0200

    Homepage and test

--- JordnærTest2/UnitTest1.cs
+++ JordnærTest2/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace JordnærTest2
         private string connection ="";
         
         [TestMethod]
-        public void AddMember()
+        public void AddMemberTest()
         {
             //arrange
             MemberService MemService = new MemberService(connection);
@@ -20,9 +20,20 @@ namespace JordnærTest2
             int Membersbefore = Members.Count();
             Member member= new Member(-2,"a","a","a","a",false,false,false);
             MemService.AddMemberAsync(member);
+            int MemberAfter= Members.Count();
+            MemService.DeleteMemberAsync(-2);
 
+            //assert
+            Assert.AreEqual(Membersbefore+1, MemberAfter);
 
+        }
 
+        [TestMethod]
+        public void DeleteMemberTest()
+        {
+            //arrange
+            MemberService MemService = new MemberService(connection);
+            List<Member> Members = MemService.GetAllMembersAsync().Result;
         }
     }
 }
---
+++ SemesterProjekt 2/Pages/Events/HomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SemesterProjekt_2.Pages.Events
+{
+    public class HomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
