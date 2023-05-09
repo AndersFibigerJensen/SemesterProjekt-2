67e7786c6a692b4087fda01d41ed1e96aef0dcaa
Mammon22 <anders99fib@gmail.com>
2023-05-09 10:08:49 +0200

    Blog

---
+++ SemesterProjekt 2/Interfaces/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using SemesterProjekt_2.Models;
+
+namespace SemesterProjekt_2.Interfaces
+{
+    public interface IBlogService
+    {
+
+        Task<Blog> CreateBlogPost();
+
+        Task UpdateBlogPost();
+
+        Task DeleteBlogPost();
+
+        Task<List<Blog>> GetBlogPosts();
+
+        Task<Blog> GetBlog(int id); 
+
+    }
+}
---
+++ SemesterProjekt 2/Models/Blog.cs
@@ -0,0 +1,27 @@
+﻿namespace SemesterProjekt_2.Models
+{
+    public class Blog
+    {
+
+        public int BlogID { get; set; }
+
+        public string BlogPost { get; set; }
+
+        public string Image { get; set;}
+
+        public Member member { get; set; }
+
+        public Blog()
+        {
+
+        }
+
+        public Blog(int blogID,string blogPost,string image,Member Member)
+        {
+            BlogID= blogID;
+            BlogPost= blogPost;
+            Image = image;
+            member= Member;
+        }
+    }
+}
