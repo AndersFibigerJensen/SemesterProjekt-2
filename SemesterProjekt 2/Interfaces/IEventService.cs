26ee759c2a86163ec793a58d62867a06457bab46
Adam Nørrind Schaumburg-Müller <adam8154@edu.zealand.dk>
2023-06-12 12:52:22 +0200

    &&&

--- SemesterProjekt 2/Interfaces/IEventService.cs
+++ SemesterProjekt 2/Interfaces/IEventService.cs
@@ -12,38 +12,38 @@ namespace SemesterProjekt_2.Interfaces
         /// <summary>
         /// Tilføjer en event til databasen
         /// </summary>
-        /// <param name="begivenhed"></param>
+        /// <param name="begivenhed">Tilføjer en event inde i parametret til databasen</param>
         /// <returns></returns>
         public Task AddEventAsync(Event begivenhed);
         /// <summary>
         /// Sletter en event fra databasen
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">Sletter en event inde i parametret fra databasen</param>
         /// <returns></returns>
         public Task<Event> RemoveEventAsync(int id);
         /// <summary>
         /// Opdaterer en event i databasen
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">Opdaterer en events properties inde i parametret fra databasen</param>
         /// <returns></returns>
         public Task<bool> UpdateEventAsync(Event eEvent, int id);
         /// <summary>
         /// Søger på efter events i databasen ud fra deres navn og viser dem, der matcher søgningen
         /// </summary>
-        /// <param name="filter"></param>
+        /// <param name="filter">Finder en eller flere events i databasen</param>
         /// <returns>event eller events, hvis navn man har søgt efter</returns>
         public Task<List<Event>> FilterEventsAsync(string filter);
         /// <summary>
         /// Finder en event med et givent id
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">Finder en event der matcher parametres id </param>
         /// <returns>id for den angivne event</returns>
         public Task<Event> GetEventByIdAsync(int id);
         /// <summary>
         /// Tilmelder et medlem til en event
         /// </summary>
-        /// <param name="eventid"></param>
-        /// <param name="memberid"></param>
+        /// <param name="eventid">Finder en event i databasen</param>
+        /// <param name="memberid">Finder et medlem i databasen</param>
         /// <returns></returns>
         public Task JoinEvent(int eventid, int memberid);
         /// <summary>
